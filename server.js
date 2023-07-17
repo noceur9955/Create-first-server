0788a95037d5cdf0dde7cd4f36d73428181a4c26
noceur9955 <akhileshkumarsingh692001@gmail.com>
2023-07-18 05:20:45 +0530

    installed 3rd party packages (nodemon)

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 8081;
 http
 .createServer((request,response)=>{
     response.writeHead(200,{"Content-Type":"text/html"});
-    response.write("<h1> Hello, this is my first server</h1>");
+    response.write("<h1> Hello,  your first server</h1>");
     response.end();
 }).listen(port,()=>{
 console.log('Nodejs server started on port ${port}');
